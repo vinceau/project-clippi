5cf2c221acd564e0156aba1f4bb024592d775642
Vince Au <vinceau09@gmail.com>
2019-11-17 17:12:23 +1100

    Simplify redux boilerplate

--- src/renderer/containers/CounterContainer.ts
+++ src/renderer/containers/CounterContainer.ts
@@ -1,17 +1,16 @@
 import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
 
 import Counter from '../components/Counter';
 import { RootState } from '../reducers';
-import { CounterAction, decrement, increment } from '../actions/counterActions';
+import { decrement, increment } from '../actions/counterActions';
 
 const mapStateToProps = (state: RootState) => ({
     value: state.counter.value
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<CounterAction>) => ({
-    incrementValue: () => dispatch(increment()),
-    decrementValue: () => dispatch(decrement())
-});
+const mapDispatchToProps = {
+    incrementValue: increment,
+    decrementValue: decrement
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
