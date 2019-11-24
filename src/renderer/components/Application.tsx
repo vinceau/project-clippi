ec868f3073f6546f7a0dbe68ccfebe334743ff42
Vince Au <vinceau09@gmail.com>
2019-11-24 14:38:36 +1100

    Clean up code

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 // import { SlippiConnect } from './SlippiConnect/SlippiConnect';
-// import { CountContainer } from './Sharks';
 import { TwitchConnect } from './TwitchConnect/TwitchConnect';
 
 const Main: React.FC<{}> = () => {
--- src/renderer/store/models/index.ts
+++ src/renderer/store/models/index.ts
@@ -1,4 +1,3 @@
-import { sharks } from './sharks';
 import { twitch } from './twitch';
 
-export { sharks, twitch };
+export { twitch };
