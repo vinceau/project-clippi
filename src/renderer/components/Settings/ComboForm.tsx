a038389b46c0b84064242ad90ab067b212d6c334
Vince Au <vinceau09@gmail.com>
2019-12-13 19:53:36 +1100

    Add final form arrays

--- src/renderer/components/Settings/ComboForm.tsx
+++ src/renderer/components/Settings/ComboForm.tsx
@@ -3,9 +3,13 @@ import * as React from 'react';
 import Styles from './Styles';
 import { Form, Field } from 'react-final-form';
 
+import arrayMutators from 'final-form-arrays';
+import { FieldArray } from 'react-final-form-arrays';
+import { ComboFilterSettings } from 'slp-realtime';
+
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-const onSubmit = async (values: any) => {
+const onSubmit = async (values: Values) => {
     await sleep(300);
     // @ts-ignore
     window.alert(JSON.stringify(values, 0, 2));
@@ -28,12 +32,26 @@ export interface ComboFilterSettings {
 
 */
 
+type Values = Partial<ComboFilterSettings>;
+
 export const ComboForm = () => (
     <Styles>
         <Form
             onSubmit={onSubmit}
-            initialValues={{ stooge: 'larry', employed: false }}
-            render={({ handleSubmit, form, submitting, pristine, values }) => (
+            mutators={{
+                ...arrayMutators
+            }}
+            initialValues={{ comboMustKill: true }}
+            render={({
+                handleSubmit,
+                form: {
+                    mutators: { push, pop }
+                },
+                submitting,
+                pristine,
+                values,
+                form
+            }) => (
                 <form onSubmit={handleSubmit}>
                     <div>
                         <label>First Name</label>
@@ -130,6 +148,39 @@ export const ComboForm = () => (
                             </label>
                         </div>
                     </div>
+                    <div className="buttons">
+                        <button type="button" onClick={() => push('customers', undefined)}>
+                            Add Customer
+                        </button>
+                        <button type="button" onClick={() => pop('customers')}>
+                            Remove Customer
+                        </button>
+                    </div>
+                    <FieldArray name="customers">
+                        {({ fields }) =>
+                            fields.map((name, index) => (
+                                <div key={name}>
+                                    <label>Cust. #{index + 1}</label>
+                                    <Field
+                                        name={`${name}.firstName`}
+                                        component="input"
+                                        placeholder="First Name"
+                                    />
+                                    <Field
+                                        name={`${name}.lastName`}
+                                        component="input"
+                                        placeholder="Last Name"
+                                    />
+                                    <span
+                                        onClick={() => fields.remove(index)}
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        ❌
+                                    </span>
+                                </div>
+                            ))
+                        }
+                    </FieldArray>
                     <div>
                         <label>Best Stooge</label>
                         <div>
