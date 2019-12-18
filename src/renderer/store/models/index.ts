0565021ddd03a904a66e1aa22104f78c8f072161
Vince Au <vinceau09@gmail.com>
2019-12-18 15:55:55 +1100

    Remove count

--- src/renderer/store/models/index.ts
+++ src/renderer/store/models/index.ts
@@ -1,22 +1,3 @@
 import { twitch } from "./twitch";
 
-// function to create a one second delay
-const delay = (time: number) => new Promise(resolve => setTimeout(() => resolve(), time));
-
-// count model
-const count = {
-  state: 0,
-  reducers: {
-    addBy(state: number, payload: number) {
-      return state + payload
-    }
-  },
-  effects: (dispatch: any) => ({
-    async addByAsync(payload: number, state: number) {
-      await delay(1000)
-      dispatch.count.addBy(1)
-    }
-  })
-};
-
-export { twitch, count };
+export { twitch };
