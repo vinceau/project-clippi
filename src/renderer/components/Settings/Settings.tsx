e2e466be95231cfd167aab9687ecaa9439a936bd
Vince Au <vinceau09@gmail.com>
2019-12-20 21:04:03 +1100

    Fix linting

--- src/renderer/components/Settings/Settings.tsx
+++ src/renderer/components/Settings/Settings.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { ComboForm } from './ComboForm';
 import { getStatic } from '@/lib/utils';
 
-export const SettingsPage: React.FC<{}> = props => {
+export const SettingsPage: React.FC<{}> = () => {
     const imgSrc = getStatic('/images/bowser_default.png');
     return (
         <div>
