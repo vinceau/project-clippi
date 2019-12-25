6c3b1fec403d66c291ba9a66c11e16b7af91f863
Vince Au <vinceau09@gmail.com>
2019-12-26 10:57:51 +1100

    Remove some any types

--- src/renderer/components/Settings/ComboForm/PerCharPercent.tsx
+++ src/renderer/components/Settings/ComboForm/PerCharPercent.tsx
@@ -1,26 +1,32 @@
 import * as React from "react";
 
+import { Character, getCharacterName } from "@vinceau/slp-realtime";
+import { produce } from "immer";
 import { Field } from "react-final-form";
 import { FieldArray } from "react-final-form-arrays";
-import { CharacterSelectAdapter } from "./CharacterSelect";
-import { produce } from "immer";
-import { getCharacterName } from "@vinceau/slp-realtime";
 
-export const mapCharacterPercentArrayToObject = (name: string, values: any): any => produce(values, draft => {
+import { CharacterSelectAdapter } from "./CharacterSelect";
+
+interface CharPercentOption {
+    character: Character;
+    percent: number;
+}
+
+export const mapCharacterPercentArrayToObject = (name: string, values: any): any => produce(values, (draft: any) => {
     const newValue = {};
-    draft[name].forEach((c: any) => {
+    draft[name].forEach((c: CharPercentOption) => {
         newValue[c.character] = c.percent;
     });
     draft[name] = newValue;
 });
 
-export const mapObjectToCharacterPercentArray = (name: string, values: any): any => produce(values, draft => {
+export const mapObjectToCharacterPercentArray = (name: string, values: any): any => produce(values, (draft: any) => {
     const charPercents = draft[name];
-    const percentArray = [];
-    for (let [key, value] of Object.entries(charPercents)) {
+    const percentArray: CharPercentOption[] = [];
+    for (const [key, value] of Object.entries(charPercents)) {
         percentArray.push({
             character: parseInt(key, 10),
-            percent: value,
+            percent: value as number,
         });
     }
     percentArray.sort((a, b) => {
@@ -33,10 +39,10 @@ export const mapObjectToCharacterPercentArray = (name: string, values: any): any
     draft[name] = percentArray;
 });
 
-
 export const PerCharPercent: React.FC<{ name: string; values: any; push: any; pop: any }> = props => {
     const { name, values, push } = props;
-    const selectedCharacters = (values[name] || []).filter((c: any) => Boolean(c)).map((c: any) => c.character);
+    const selectedChars: CharPercentOption[] = values[name] || [];
+    const selectedCharIDs = selectedChars.filter(c => Boolean(c)).map(c => c.character);
     return (
         <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
             <FieldArray name={name}>
@@ -46,9 +52,9 @@ export const PerCharPercent: React.FC<{ name: string; values: any; push: any; po
                         <div key={n} style={{display: "flex", flexDirection: "row"}}>
                             <CharacterSelectAdapter
                                 name={`${n}.character`}
-                                disabledOptions={selectedCharacters}
+                                disabledOptions={selectedCharIDs}
                             />
-                            <Field name={`${n}.percent`} component="input" type="number" parse={(v: any) => parseInt(v, 10)} />
+                            <Field name={`${n}.percent`} component="input" type="number" parse={(v: string) => parseInt(v, 10)} />
                             <span
                                 onClick={() => fields.remove(index)}
                                 style={{ cursor: "pointer" }}
