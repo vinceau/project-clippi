3530cb5229bc0daff1985bcc928cdeacc62ef38e
Vince Au <vinceau09@gmail.com>
2019-12-27 17:55:13 +1100

    Use shortname provided in slp-realtime

--- src/renderer/components/CharacterIcon.tsx
+++ src/renderer/components/CharacterIcon.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Character } from "@vinceau/slp-realtime";
-import { getStatic } from "@/lib/utils";
 import styled from "styled-components";
+
+import { getStatic } from "@/lib/utils";
+import { Character, getCharacterShortName } from "@vinceau/slp-realtime";
 
 // const pathToCats = require.context("../../../styles/images/icons/chars", true);
 export const CharacterIcon: React.FC<{
@@ -21,62 +22,5 @@ export const CharacterIcon: React.FC<{
 };
 
 const characterToFilename = (character: Character): string => {
-    return `${characterToShortname(character)}_default.png`;
-};
-
-const characterToShortname = (character: Character): string => {
-    switch (character) {
-        case Character.CAPTAIN_FALCON:
-            return "falcon";
-        case Character.DONKEY_KONG:
-            return "dk";
-        case Character.FOX:
-            return "fox";
-        case Character.GAME_AND_WATCH:
-            return "gnw";
-        case Character.KIRBY:
-            return "kirby";
-        case Character.BOWSER:
-            return "bowser";
-        case Character.LINK:
-            return "link";
-        case Character.LUIGI:
-            return "luigi";
-        case Character.MARIO:
-            return "mario";
-        case Character.MARTH:
-            return "marth"
-        case Character.MEWTWO:
-            return "mewtwo";
-        case Character.NESS:
-            return "ness";
-        case Character.PEACH:
-            return "peach";
-        case Character.PIKACHU:
-            return "pikachu";
-        case Character.ICE_CLIMBERS:
-            return "ics";
-        case Character.JIGGLYPUFF:
-            return "puff";
-        case Character.SAMUS:
-            return "samus";
-        case Character.YOSHI:
-            return "yoshi";
-        case Character.ZELDA:
-            return "zelda";
-        case Character.SHEIK:
-            return "sheik";
-        case Character.FALCO:
-            return "falco";
-        case Character.YOUNG_LINK:
-            return "yl";
-        case Character.DR_MARIO:
-            return "doc";
-        case Character.ROY:
-            return "roy";
-        case Character.PICHU:
-            return "pichu";
-        case Character.GANONDORF:
-            return "ganon";
-    }
+    return `${getCharacterShortName(character)}_default.png`;
 };
