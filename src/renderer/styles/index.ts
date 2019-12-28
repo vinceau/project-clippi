0c6d3097d78003252d67a77fc75a6ee8be524615
Vince Au <vinceau09@gmail.com>
2019-12-28 14:10:50 +1100

    Add semantic ui

--- src/renderer/styles/index.ts
+++ src/renderer/styles/index.ts
@@ -1,9 +1,13 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCog, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-import "./index.scss";
+// Import the fontawesome icons
+library.add(faCog, faTimes);
 
 // React-tippy styles
-import 'react-tippy/dist/tippy.css'
+import "react-tippy/dist/tippy.css";
 
-library.add(faCog, faTimes);
+// Semantic UI styles
+import "semantic-ui-css/semantic.min.css";
+
+import "./index.scss";
