9123eea6ae5f6052eb956793e50362495019651e
Vince Au <vinceau09@gmail.com>
2019-12-30 20:04:07 +1100

    Update sound settings page

--- src/renderer/components/Settings/SoundSettings.tsx
+++ src/renderer/components/Settings/SoundSettings.tsx
@@ -7,6 +7,7 @@ import { Button, Icon, Table } from "semantic-ui-react";
 import { Dispatch, iRootState } from "@/store";
 import { sp } from "@/lib/sounds";
 import { getFilePath } from "@/lib/utils";
+import styled from "styled-components";
 
 
 export const SoundSettings: React.FC = () => {
@@ -30,14 +31,19 @@ export const SoundSettings: React.FC = () => {
         sp.removeSound(name);
         dispatch.filesystem.setSoundFiles(sp.serialize());
     };
+    const Buttons = styled.div`
+    margin-bottom: 10px;
+    `;
     return (
         <div>
-            <h1>Sound Settings</h1>
-            <Button onClick={() => getSounds().catch(console.error)}>
-                <Icon name="add" />
-                Add sounds
+            <h2>Sounds</h2>
+            <Buttons>
+                <Button onClick={() => getSounds().catch(console.error)}>
+                    <Icon name="add" />
+                    Add sound
             </Button>
-            <SoundTable onPlay={onPlay} onStop={() => sp.stop()} onRemove={removeSound} sounds={sounds}/>
+            </Buttons>
+            <SoundTable onPlay={onPlay} onStop={() => sp.stop()} onRemove={removeSound} sounds={sounds} />
         </div>
     );
 };
@@ -66,42 +72,42 @@ const SoundTable: React.FC<{
     };
     props.sounds.forEach((value, key) => {
         rows.push((
-      <Table.Row key={`${value}--${key}`}>
-        <Table.Cell>
-            {key}
-        </Table.Cell>
-        <Table.Cell>
-          {value}
-        </Table.Cell>
-        <Table.Cell>
-            {playing === key ?
-            <span onClick={() => onStop()}><Icon name="stop" /></span>
-            :
-            <span onClick={() => onPlay(key)}><Icon name="play"/></span>
-    }
-        </Table.Cell>
-        <Table.Cell>
-            <span onClick={() => onRemove(key)}><Icon name="remove"/></span>
-        </Table.Cell>
-      </Table.Row>
+            <Table.Row key={`${value}--${key}`}>
+                <Table.Cell>
+                    {key}
+                </Table.Cell>
+                <Table.Cell>
+                    {value}
+                </Table.Cell>
+                <Table.Cell>
+                    {playing === key ?
+                        <span onClick={() => onStop()}><Icon name="stop" /></span>
+                        :
+                        <span onClick={() => onPlay(key)}><Icon name="play" /></span>
+                    }
+                </Table.Cell>
+                <Table.Cell>
+                    <span onClick={() => onRemove(key)}><Icon name="remove" /></span>
+                </Table.Cell>
+            </Table.Row>
         ));
     });
     return (
         <div>
- <Table celled padded>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell singleLine>Name</Table.HeaderCell>
-        <Table.HeaderCell>File Path</Table.HeaderCell>
-        <Table.HeaderCell>Play</Table.HeaderCell>
-        <Table.HeaderCell>Delete</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+            <Table celled padded>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell singleLine>Name</Table.HeaderCell>
+                        <Table.HeaderCell>File Path</Table.HeaderCell>
+                        <Table.HeaderCell>Play</Table.HeaderCell>
+                        <Table.HeaderCell>Delete</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
 
-    <Table.Body>
-        {rows}
-    </Table.Body>
-  </Table>
+                <Table.Body>
+                    {rows}
+                </Table.Body>
+            </Table>
         </div>
     );
 };
