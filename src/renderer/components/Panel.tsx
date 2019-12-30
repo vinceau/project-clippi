c3ce7d78659d9a61f4e00ca729d4e0f295cb7669
Vince Au <vinceau09@gmail.com>
2019-12-30 18:56:35 +1100

    Remove experimental sound playing code

--- src/renderer/components/Panel.tsx
+++ src/renderer/components/Panel.tsx
@@ -15,74 +15,6 @@ const noop = () => {
     console.log("inside callback");
 };
 
-const FileInput: React.FC<{
-    value: string[];
-    onChange: any;
-}> = ({ value, onChange = noop, ...rest }) => {
-    console.log(value);
-    return (
-        <div>
-            {Boolean(value && value.length) && (
-                <div>Selected files: {
-                    value.map((fname: string) => {
-                        return <div key={fname} onClick={(e) => {
-                            e.preventDefault();
-                            console.log(`playing: ${fname}`);
-                            sp.playSound(fname).catch(console.error);
-                            console.log(`done playing ${fname}`);
-                        }}>Name: {fname}</div>
-                    })
-                }</div>
-            )}
-            <label>
-                Click to select some files...
-      <input
-                    {...rest}
-                    style={{ display: "none" }}
-                    type="file"
-                    onChange={(e: any) => {
-                        console.log([...e.target.files]);
-                        onChange([...e.target.files]);
-                    }}
-                />
-            </label>
-        </div>
-    );
-};
-
-
-const SoundPlayer = () => {
-    let sound: Howl | null = null;
-    const onChange = (event: any) => {
-        const files = event.target.files;
-        // Read the file from the input
-        if (files.length > 0) {
-            const file = files[0];
-            const reader = new FileReader();
-            reader.addEventListener("load", () => {
-                const data = reader.result;
-                // Create a Howler sound
-                sound = new Howl({
-                    src: data,
-                    format: file.name.split(".").pop().toLowerCase(), // always give file extension: this is optional but helps
-                });
-            });
-            reader.readAsDataURL(file);
-        }
-    };
-    const playSound = () => {
-        if (sound) {
-            sound.play();
-        }
-    }
-    return (
-        <div>
-            <input type="file" onChange={onChange} />
-            <button onClick={playSound}>play</button>
-        </div>
-    );
-};
-
 const Count = () => {
     const slippiPort = useSelector((state: iRootState) => state.slippi.port);
     const authToken = useSelector((state: iRootState) => state.twitch.authToken);
@@ -105,27 +37,6 @@ const Count = () => {
         connectToSlippi(parseInt(port, 10)).catch(console.error);
     };
 
-    const [fileValue, setFileValue] = React.useState<string[]>([]);
-    const fileSelect = (f: any[]) => {
-        console.log(`got back ${f}`);
-        const fl = [];
-        for (const ff of f) {
-            console.log(ff);
-            sp.addSound(ff.name, ff.path);
-            fl.push(ff.name);
-
-            const reader = new FileReader();
-            reader.addEventListener("load", () => {
-                const data = reader.result;
-                console.log(data);
-            });
-            reader.readAsDataURL(ff);
-        }
-        console.log(f);
-        setFileValue(fl);
-
-    };
-
     return (
         <Outer>
             <div style={{ width: 120 }}>
@@ -133,8 +44,6 @@ const Count = () => {
                 <SlippiPage initialPort={slippiPort} onSubmit={handleConnect} />
             </div>
             <button onClick={handleClick}>notify</button>
-            <SoundPlayer />
-            <FileInput value={fileValue} onChange={fileSelect} />
             {authToken ?
                 <TwitchClip accessToken={authToken} />
                 :
