bf06fcf148102ea0deb11b2ed8da5016223c2980
Vince Au <vinceau09@gmail.com>
2020-01-02 09:07:05 +1100

    Add CodeBlock

---
+++ src/renderer/components/Settings/CodeBlock.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import { isDevelopment } from "@/lib/utils";
+
+export const CodeBlock: React.FC<{
+    values: any
+}> = (props) => {
+    if (isDevelopment) {
+        return (<pre>{(JSON as any).stringify(props.values, 0, 2)}</pre>);
+    }
+    return null;
+};
