6a9fadc6afd846990e193967558d4c3cf082e4b1
Vince Au <vinceau09@gmail.com>
2020-01-03 17:35:12 +1100

    Add boiler plate for obs form

--- src/renderer/components/Settings/OBSSettings.tsx
+++ src/renderer/components/Settings/OBSSettings.tsx
@@ -1,7 +1,26 @@
 import * as React from "react";
 
+import { Button, Form } from "semantic-ui-react";
+
 export const OBSSettings = () => {
     return (<div>
         <h2>OBS Configuration</h2>
+        <div style={{ maxWidth: "500px" }}>
+            <Form>
+                <Form.Field>
+                    <label>IP Address</label>
+                    <input placeholder="localhost" />
+                </Form.Field>
+                <Form.Field>
+                    <label>Port</label>
+                    <input placeholder="4444" />
+                </Form.Field>
+                <Form.Field>
+                    <label>Websocket Password</label>
+                    <input placeholder="Password" />
+                </Form.Field>
+                <Button type="submit">Connect</Button>
+            </Form>
+        </div>
     </div>);
 };
