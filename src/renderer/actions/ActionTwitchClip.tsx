e8863010fffc70e09affedc3ad43f6f7e9d4fd3c
Vince Au <vinceau09@gmail.com>
2020-01-04 23:38:11 +1100

    Add option to notify after Twitch clip

--- src/renderer/actions/ActionTwitchClip.tsx
+++ src/renderer/actions/ActionTwitchClip.tsx
@@ -4,14 +4,14 @@ import { ActionTypeGenerator } from "@vinceau/event-actions";
 import { produce } from "immer";
 import { Checkbox, Icon } from "semantic-ui-react";
 
-import { notify } from "@/lib/utils";
+import { notify as sendNotification } from "@/lib/utils";
 import { createTwitchClip } from "common/twitch";
 import { dispatcher, store } from "../store";
 import { ActionComponent } from "./types";
 
 interface ActionCreateTwitchClipParams {
     delay?: boolean;
-    // notify?: boolean;
+    notify?: boolean;
 }
 
 const actionCreateClip: ActionTypeGenerator = (params: ActionCreateTwitchClipParams) => {
@@ -21,17 +21,20 @@ const actionCreateClip: ActionTypeGenerator = (params: ActionCreateTwitchClipPar
         console.log(params);
         console.log(`creating clip with token: ${token}`);
         try {
-            const clipID = await createTwitchClip(token, params.delay);
+            const clipID = await createTwitchClip(token, params.delay, "n3zmodgod");
             // Get timestamp in seconds
             const timestamp = (new Date()).getTime() / 1000;
             dispatcher.twitch.addTwitchClip({
                 clipID,
                 timestamp,
             });
+            if (params.notify) {
+                sendNotification(`Clipped ${clipID}`, "Twitch clip created");
+            }
             return clipID;
         } catch (err) {
             console.error(err);
-            notify("Failed to create Twitch clip. Are you sure you are live?");
+            sendNotification("Failed to create Twitch clip. Are you sure you are live?");
             return null;
         }
     };
@@ -51,15 +54,31 @@ const TwitchClipInput = (props: any) => {
         });
         onChange(newValue);
     };
-    const toggle = () => onDelayChange(!value.delay);
+    const toggleDelay = () => onDelayChange(!value.delay);
+    const onNotifyChange = (notify?: boolean) => {
+        const newValue = produce(value, (draft: ActionCreateTwitchClipParams) => {
+            draft.notify = notify;
+        });
+        onChange(newValue);
+    };
+    const toggleNotify = () => onNotifyChange(!value.notify);
 
     return (
         <div>
-            <Checkbox
-                label="Delay before clipping"
-                onChange={toggle}
-                checked={value.delay}
-            />
+            <div style={{ marginBottom: "10px" }}>
+                <Checkbox
+                    label="Delay before clipping"
+                    onChange={toggleDelay}
+                    checked={value.delay}
+                />
+            </div>
+            <div>
+                <Checkbox
+                    label="Notify after clipping"
+                    onChange={toggleNotify}
+                    checked={value.notify}
+                />
+            </div>
         </div>
     );
 };
