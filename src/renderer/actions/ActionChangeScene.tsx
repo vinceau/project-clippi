c68a522e69c9e4f2c82423a39e43cfa16577c2e6
Vince Au <vinceau09@gmail.com>
2020-01-05 00:08:01 +1100

    Add delay before changing scenes

--- src/renderer/actions/ActionChangeScene.tsx
+++ src/renderer/actions/ActionChangeScene.tsx
@@ -3,24 +3,61 @@ import * as React from "react";
 import { ActionTypeGenerator } from "@vinceau/event-actions";
 import { produce } from "immer";
 import { useSelector } from "react-redux";
-import { Button } from "semantic-ui-react";
+import { Form, Input, Button } from "semantic-ui-react";
 
 import { InlineDropdown } from "@/components/Misc/InlineInputs";
 import { CustomIcon } from "@/components/Misc/Misc";
 import { connectToOBSAndNotify, getAllScenes, setScene } from "@/lib/obs";
 import { iRootState } from "@/store";
 import { ActionComponent } from "./types";
+import { notify, delay as waitMillis } from "@/lib/utils";
 
 import obsIcon from "@/styles/images/obs.svg";
-import { notify } from "@/lib/utils";
+import styled from "styled-components";
 
 interface ActionChangeSceneParams {
     scene: string;
+    delay?: string;  // delay in milliseconds string
 }
+
+const CustomInput = styled(Input)`
+    &&& {
+        & > input {
+            text-align: right;
+            width: 50px !important;
+        }
+    }
+    `;
+
+export const DelayInput: React.FC<{
+    value: string;
+    onChange: (delay: string) => void;
+}> = props => {
+    const [delayAmount, setDelayAmount] = React.useState(props.value || "0");
+    return (
+        <Form>
+            <Form.Field inline>
+                <span>After waiting: </span>
+                <CustomInput
+                    transparent={true}
+                    value={delayAmount}
+                    onBlur={() => props.onChange(delayAmount)}
+                    onChange={(_: any, data: { value: string }) => {
+                        setDelayAmount(data.value);
+                    }} placeholder="2500" />
+                <span>ms</span>
+            </Form.Field>
+        </Form>
+    );
+};
 
 const actionChangeScene: ActionTypeGenerator = (params: ActionChangeSceneParams) => {
     return async (): Promise<void> => {
         try {
+            const millis = parseInt(params.delay || "0", 10);
+            if (millis > 0) {
+                await waitMillis(millis);
+            }
             await setScene(params.scene);
         } catch (err) {
             console.error(err);
@@ -55,13 +92,24 @@ const SceneNameInput = (props: any) => {
         });
         onChange(newValue);
     };
+
+    const onDelayChange = (delay: string) => {
+        const newValue = produce(value, (draft: ActionChangeSceneParams) => {
+            draft.delay = delay;
+        });
+        onChange(newValue);
+    };
+
     return (
-        <InlineDropdown
-            value={value.scene}
-            prefix="Scene: "
-            onChange={onSceneChange}
-            customOptions={allScenes}
-        />
+        <div>
+            <InlineDropdown
+                value={value.scene}
+                prefix="Scene: "
+                onChange={onSceneChange}
+                customOptions={allScenes}
+            />
+            <DelayInput value={value.delay} onChange={onDelayChange} />
+        </div>
     );
 };
 
