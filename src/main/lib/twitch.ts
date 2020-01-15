3e0e408b2a0c91619822cad1723faa920d84d1fd
Vince Au <vinceau09@gmail.com>
2020-01-16 00:11:37 +1100

    Hard-code the Twitch redirect URI

--- src/main/lib/twitch.ts
+++ src/main/lib/twitch.ts
@@ -2,11 +2,15 @@ import twitchElectronAuthProvider from "twitch-electron-auth-provider";
 
 import { deleteCookie, fetchCookies } from "./session";
 
+const TWITCH_REDIRECT_URI = "http://localhost:3000/auth/twitch/callback";
+
 const clientId = process.env.ELECTRON_WEBPACK_APP_TWITCH_CLIENT_ID || "";
-const redirectURI = process.env.ELECTRON_WEBPACK_APP_TWITCH_REDIRECT_URI || "";
 
 export const authenticateTwitch = async (scopes: string | string[]): Promise<string> => {
-    const provider = new twitchElectronAuthProvider({ clientId, redirectURI });
+    const provider = new twitchElectronAuthProvider({
+        clientId,
+        redirectURI: TWITCH_REDIRECT_URI,
+    });
     const tok = await provider.getAccessToken(scopes);
     return tok.accessToken;
 };
