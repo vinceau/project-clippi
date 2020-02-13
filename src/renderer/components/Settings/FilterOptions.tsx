98083ef054263b14397019a9d32974f79f061cb7
Vince Au <vinceau09@gmail.com>
2020-02-14 00:18:52 +1100

    Don't set combo settings multiple times.
    The combo settings are already reflected every time the store
    changes. Thus, there is no need to continue to update the settings
    especially inside the render function.

--- src/renderer/components/Settings/FilterOptions.tsx
+++ src/renderer/components/Settings/FilterOptions.tsx
@@ -3,25 +3,18 @@ import * as React from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { comboFilter } from "@/lib/realtime";
 import { Dispatch, iRootState } from "@/store";
 import { ComboForm } from "./ComboForm/ComboForm";
 import { ProfileSelector } from "./ComboForm/ProfileSelection";
-import { mapConfigurationToFilterSettings, ComboConfiguration } from "@/lib/profile";
+import { ComboConfiguration } from "@/lib/profile";
 
 export const FilterOptions = () => {
     const { currentProfile, comboProfiles } = useSelector((state: iRootState) => state.slippi);
     const profileOptions = Object.keys(comboProfiles);
+    const initial: ComboConfiguration = JSON.parse(comboProfiles[currentProfile]);
 
-    let initial = comboFilter.getSettings();
-    const slippiSettings = comboProfiles[currentProfile];
-    const converted = mapConfigurationToFilterSettings(JSON.parse(slippiSettings));
-    if (slippiSettings) {
-        initial = comboFilter.updateSettings(converted);
-    }
     const dispatch = useDispatch<Dispatch>();
     const onSubmit = (values: Partial<ComboConfiguration>) => {
-        // console.log(values);
         const valueString = JSON.stringify(values);
         dispatch.slippi.saveProfile({
             name: currentProfile,
