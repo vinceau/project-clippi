94b1790de2906cc5de5db51c8e7343234464b573
Vince Au <vinceau09@gmail.com>
2020-02-22 12:17:16 +1100

    Pass props to styled components

--- src/renderer/components/layout/MenuIcon.tsx
+++ src/renderer/components/layout/MenuIcon.tsx
@@ -2,44 +2,41 @@ import * as React from "react";
 
 import styled from "styled-components";
 
+import { Link, Route } from "react-router-dom";
 import { Tooltip } from "react-tippy";
-import { Icon } from "semantic-ui-react";
 
-import {
-    BrowserRouter as Router,
-    Link,
-    Route
-} from "react-router-dom";
+const OuterMenuIcon = styled.div<{
+    active?: boolean;
+}>`
+    position: relative;
+    height: 70px;
+    width: 100%;
+    color: rgba(255, 255, 255, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+    ${(props) => props.active && `
+    color: white;
+    border-left: solid 4px white;
+    background-color: rgba(255, 255, 255, 0.1);
+    `}
+
+    &:hover {
+        color: rgba(255, 255, 255, 0.75);
+        background-color: rgba(255, 255, 255, 0.05);
+    }
+`;
 
 export const MenuIcon: React.FC<{
     active?: boolean;
     label?: string;
 }> = (props) => {
-    const Outer = styled.div`
-        position: relative;
-        height: 70px;
-        width: 100%;
-        color: rgba(255, 255, 255, 0.5);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 25px;
-        ${props.active && `
-        color: white;
-        border-left: solid 4px white;
-        background-color: rgba(255, 255, 255, 0.1);
-        `}
-
-        &:hover {
-            color: rgba(255, 255, 255, 0.75);
-            background-color: rgba(255, 255, 255, 0.05);
-        }
-    `;
     return (
         <Tooltip title={props.label} size="big" distance={-70} duration={200} position="right" style={{ display: "inline-block", width: "100%" }}>
-            <Outer>
+            <OuterMenuIcon active={props.active}>
                 {props.children}
-            </Outer>
+            </OuterMenuIcon>
         </Tooltip>
     );
 };
