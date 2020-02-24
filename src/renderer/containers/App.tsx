a2fc31ab9f038f93b5c2b096add7e0b9d8687ddc
Vince Au <vinceau09@gmail.com>
2020-02-24 21:59:09 +1100

    Fix theme not applying

--- src/renderer/containers/App.tsx
+++ src/renderer/containers/App.tsx
@@ -8,7 +8,6 @@ import { ThemeProvider } from "styled-components";
 import { ThemeManager, lightTheme, darkTheme, useTheme, ThemeMode } from "@/styles";
 import { SettingsPage } from "@/containers/Settings/Settings";
 
-
 const Settings = () => {
     const history = useHistory();
     return (
@@ -18,25 +17,28 @@ const Settings = () => {
 
 const App: React.FC = () => {
     const theme = useTheme();
+    return (
+        <ThemeProvider theme={theme.themeName === ThemeMode.LIGHT ? lightTheme : darkTheme}>
+            <Router>
+                <Switch>
+                    <Route path="/main" component={MainView} />
+                    <Route path="/settings" component={Settings} />
+                    <Route exact path="/">
+                        <Redirect to="/main" />
+                    </Route>
+                </Switch>
+            </Router>
+        </ThemeProvider>
+    );
+};
 
+const ThemedApp: React.FC = () => {
+    // ThemedManager must be declared and instantiated before useTheme() is called
     return (
         <ThemeManager>
-            <ThemeProvider theme={theme.themeName === ThemeMode.LIGHT ? lightTheme : darkTheme}>
-                <div>
-                    <Router>
-                        <Switch>
-                            <Route path="/main" component={MainView} />
-                            <Route path="/settings" component={Settings} />
-                            <Route exact path="/">
-                                <Redirect to="/main" />
-                            </Route>
-                        </Switch>
-                    </Router>
-
-                </div>
-            </ThemeProvider>
+            <App />
         </ThemeManager>
     );
 };
 
-export default hot(App);
+export default hot(ThemedApp);
