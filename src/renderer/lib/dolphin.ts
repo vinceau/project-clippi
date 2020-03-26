b6f4da26cbcbd190fd088ea52d739b93a6fd9ad0
Nikhil Narayana <nikhil.narayana@live.com>
2020-03-26 01:09:51 -0400

    swap to a switch statement

--- src/renderer/lib/dolphin.ts
+++ src/renderer/lib/dolphin.ts
@@ -41,52 +41,57 @@ export const openComboInDolphin = (comboFilePath: string): void => {
 };
 
 const dolphinStdoutHandler = (line: string) => {
-    const commands: string[] = line.split("\r\n").filter((value: string) => value);
+    const commands: string[] = line.split("\r\n").filter((value: string) => value); // this only runs on windows so CRLF is fine for now
     commands.forEach((command: string) => {
         const commandPair: string[] = command.split(" ");
-        if ("[CURRENT_FRAME]" === commandPair[0]) {
-            dolphinState.currentFrame = parseInt(commandPair[1]);
-            if (dolphinState.currentFrame === dolphinState.startRecordingFrame) {
-                if (!dolphinState.recordingStarted) {
-                    console.log("Start Recording");
-                    setRecordingState(OBSRecordingAction.START)
-                    dolphinState.recordingStarted = true;
-                } else {
-                    console.log("Resuming Recording");
-                    setRecordingState(OBSRecordingAction.UNPAUSE);
+        switch(commandPair[0]) {
+            case ("[CURRENT_FRAME]"):
+                dolphinState.currentFrame = parseInt(commandPair[1]);
+                if (dolphinState.currentFrame === dolphinState.startRecordingFrame) {
+                    if (!dolphinState.recordingStarted) {
+                        console.log("Start Recording");
+                        setRecordingState(OBSRecordingAction.START)
+                        dolphinState.recordingStarted = true;
+                    } else {
+                        console.log("Resuming Recording");
+                        setRecordingState(OBSRecordingAction.UNPAUSE);
+                    }
+                    
+                } else if (dolphinState.currentFrame === dolphinState.endRecordingFrame) {
+                    console.log("Pausing Recording");
+                    setRecordingState(OBSRecordingAction.PAUSE);
+                    resetState();
                 }
-                
-            } else if (dolphinState.currentFrame === dolphinState.endRecordingFrame) {
-                console.log("Pausing Recording");
-                setRecordingState(OBSRecordingAction.PAUSE);
-                resetState();
-            }
-        } else if ("[PLAYBACK_START_FRAME]" === commandPair[0]) {
-            dolphinState.startRecordingFrame = parseInt(commandPair[1]);
-            dolphinState.startRecordingFrame += 90;
-            console.log(`StartFrame: ${dolphinState.startRecordingFrame}`);
-        } else if ("[GAME_END_FRAME]" === commandPair[0]) {
-            dolphinState.lastGameFrame = parseInt(commandPair[1]);
-            console.log(`LastFrame: ${dolphinState.lastGameFrame}`);
-        } else if ("[PLAYBACK_END_FRAME]" === commandPair[0]) {
-            dolphinState.endRecordingFrame = parseInt(commandPair[1]);
-            if (dolphinState.endRecordingFrame + 120 < dolphinState.lastGameFrame) {
-                dolphinState.endRecordingFrame -= 60;
-            } else {
-                dolphinState.endRecordingFrame = dolphinState.lastGameFrame;
-            }
-            console.log(`EndFrame: ${dolphinState.endRecordingFrame}`);
-        } else if ("[NO_GAME]" === commandPair[0]) {
-            console.log("No games remaining in queue");
-            console.log("Stopping Recording");
-
-            setRecordingState(OBSRecordingAction.STOP);
-            dolphinState.recordingStarted = false;
-        } else if ("" === commandPair[0]) {
-            // whatever
-        } 
-        else {
-            console.log(`Unknown Command: ${line}`);
+                break;
+            case ("[PLAYBACK_START_FRAME]"):
+                dolphinState.startRecordingFrame = parseInt(commandPair[1]);
+                dolphinState.startRecordingFrame += 90;
+                console.log(`StartFrame: ${dolphinState.startRecordingFrame}`);
+                break;
+            case ("[GAME_END_FRAME]"):
+                dolphinState.lastGameFrame = parseInt(commandPair[1]);
+                console.log(`LastFrame: ${dolphinState.lastGameFrame}`);
+                break;
+            case ("[PLAYBACK_END_FRAME]"):
+                dolphinState.endRecordingFrame = parseInt(commandPair[1]);
+                if (dolphinState.endRecordingFrame + 120 < dolphinState.lastGameFrame) {
+                    dolphinState.endRecordingFrame -= 60;
+                } else {
+                    dolphinState.endRecordingFrame = dolphinState.lastGameFrame;
+                }
+                console.log(`EndFrame: ${dolphinState.endRecordingFrame}`);
+                break;
+            case ("[NO_GAME]"):
+                console.log("No games remaining in queue");
+                console.log("Stopping Recording");
+                setRecordingState(OBSRecordingAction.STOP);
+                dolphinState.recordingStarted = false;
+                break;
+            case (""):
+                break
+            default:
+                console.log(`Unknown Command: ${line}`);
+                break;
         }
     });
 }
