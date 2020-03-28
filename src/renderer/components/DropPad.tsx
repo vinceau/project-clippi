8708b1868fe51809859a56e5c7acc2a0cae9e2f1
Vince Au <vinceau09@gmail.com>
2020-03-29 01:01:14 +1100

    Style the drop pad

--- src/renderer/components/DropPad.tsx
+++ src/renderer/components/DropPad.tsx
@@ -1,6 +1,20 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
+import styled from "styled-components";
+
+const Outer = styled.div`
+min-height: 200px;
+max-height: 400px;
+width: 100%;
+border: solid 1px white;
+display: flex;
+align-items: center;
+justify-content: center;
+border-radius: 5px;
+cursor: pointer;
+`;
+
 export const DropPad: React.FC<{
     onFiles: (files: any) => void;
 }> = (props) => {
@@ -9,13 +23,13 @@ export const DropPad: React.FC<{
     }, []);
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
     return (
-        <div {...getRootProps()}>
+        <Outer {...getRootProps()}>
             <input {...getInputProps()} />
             {
                 isDragActive ?
                     <p>Drop the files here ...</p> :
                     <p>Drag 'n' drop some files here, or click to select files</p>
             }
-        </div>
+        </Outer>
     );
 };
