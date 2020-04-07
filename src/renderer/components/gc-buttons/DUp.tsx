8a0cd53f177b816308c46edcfa22df1b548c0fc8
Vince Au <vinceau09@gmail.com>
2020-04-07 23:47:19 +1000

    Add dpad up

---
+++ src/renderer/components/gc-buttons/DUp.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import styled from "styled-components";
+
+export const DUp: React.FC<{
+    pressed?: boolean;
+    color?: string;
+    onClick?: (value: boolean) => void;
+}> = (props) => {
+    const { pressed } = props;
+    const onClick = () => {
+        if (props.onClick) {
+            props.onClick(!pressed);
+        }
+    };
+    const buttonColor = props.color ? props.color : "#8F8F8F";
+    const Outer = styled.div`
+        width: 5em;
+        height: 6em;
+        border-top-left-radius: 1em;
+        border-top-right-radius: 1em;
+        background-color: ${pressed ? buttonColor : "transparent"}
+        border: solid 0.4em ${buttonColor};
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 1em 0.5em;
+        ${props.onClick && "cursor: pointer"}
+        svg {
+            overflow: visible;
+        }
+    `;
+    const strokeColor = pressed ? "white" : buttonColor;
+    const fillColor = pressed ? buttonColor : "none";
+    return (
+        <Outer onClick={onClick}>
+            <svg height="100%" viewBox="0 0 32 34" version="1.1">
+                <g id="Page-1" stroke={strokeColor} strokeWidth="0.3em" fill="none" fillRule="evenodd">
+                    <g id="ButtonIcon-GCN-D-Pad" transform="translate(-80.000000, -16.000000)" fill={fillColor} fillRule="nonzero">
+                        <path d="M96,16.822266 C95.13509,16.822266 94.27017,17.126858 94,17.736328 L80.87891,47.335938 C81.24118,48.237056 81.98169,49.035156 82.77539,49.035156 L109.22461,49.035156 C110.01831,49.035156 110.75882,48.237056 111.12109,47.335938 L98,17.736328 C97.72983,17.126858 96.86491,16.822266 96,16.822266 Z" id="path4219" />
+                    </g>
+                </g>
+            </svg>
+        </Outer>
+    );
+};
--- src/renderer/components/gc-buttons/GCButtons.tsx
+++ src/renderer/components/gc-buttons/GCButtons.tsx
@@ -7,6 +7,7 @@ import { LTrigger } from "./LTrigger";
 import { RTrigger } from "./RTrigger";
 import { DLeft } from "./DLeft";
 import { DRight } from "./DRight";
+import { DUp } from "./DUp";
 
 export const GCButtons: React.FC = () => {
     const [zPressed, setZPressed] = React.useState(false);
@@ -16,8 +17,10 @@ export const GCButtons: React.FC = () => {
     const [rPressed, setRPressed] = React.useState(false);
     const [dlPressed, setDLPressed] = React.useState(false);
     const [drPressed, setDRPressed] = React.useState(false);
+    const [duPressed, setDUPressed] = React.useState(false);
     return (
         <div>
+            <DUp pressed={duPressed} onClick={setDUPressed}/>
             <DRight pressed={drPressed} onClick={setDRPressed}/>
             <DLeft pressed={dlPressed} onClick={setDLPressed}/>
             <LTrigger pressed={lPressed} onClick={setLPressed}/>
