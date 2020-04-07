c468e6f7ce95b65770123bde6d61e074986ce63d
Vince Au <vinceau09@gmail.com>
2020-04-07 22:30:25 +1000

    Add dpad left

--- src/renderer/components/gc-buttons/GCButtons.tsx
+++ src/renderer/components/gc-buttons/GCButtons.tsx
@@ -5,6 +5,7 @@ import { YButton } from "./YButton";
 import { XButton } from "./XButton";
 import { LTrigger } from "./LTrigger";
 import { RTrigger } from "./RTrigger";
+import { DLeft } from "./DLeft";
 
 export const GCButtons: React.FC = () => {
     const [zPressed, setZPressed] = React.useState(false);
@@ -12,8 +13,10 @@ export const GCButtons: React.FC = () => {
     const [yPressed, setYPressed] = React.useState(false);
     const [lPressed, setLPressed] = React.useState(false);
     const [rPressed, setRPressed] = React.useState(false);
+    const [dlPressed, setDLPressed] = React.useState(false);
     return (
         <div>
+            <DLeft pressed={dlPressed} onClick={setDLPressed}/>
             <LTrigger pressed={lPressed} onClick={setLPressed}/>
             <RTrigger pressed={rPressed} onClick={setRPressed}/>
             <XButton pressed={xPressed} onClick={setXPressed}/>
