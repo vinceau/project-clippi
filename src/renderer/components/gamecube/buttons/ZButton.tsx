7c1c939c3f9f55580fbc5de73941b480956ae4e7
Vince Au <vinceau09@gmail.com>
2020-04-08 23:08:15 +1000

    Add hover state to Z button

--- src/renderer/components/gamecube/buttons/ZButton.tsx
+++ src/renderer/components/gamecube/buttons/ZButton.tsx
@@ -8,7 +8,8 @@ export const ZButton: React.FC<{
     onClick?: () => void;
 }> = (props) => {
     const { pressed, onClick } = props;
-    const buttonColor = props.color ? props.color : "#6F1FFF";
+    const buttonColor = props.color || "#6F1FFF";
+    const textColor = "white";
     const Outer = styled.div`
         width: 18em;
         height: 5em;
@@ -18,15 +19,25 @@ export const ZButton: React.FC<{
         display: flex;
         justify-content: center;
         align-items: center;
-        ${onClick && "cursor: pointer"}
-    `;
-    const Text = styled.span`
-        font-size: 3.5em;
-        color: ${pressed ? "white" : buttonColor}
+        ${onClick && `
+            cursor: pointer;
+            &:hover {
+                ${pressed ? "opacity: 0.85;" : `
+                background-color: ${buttonColor};
+                span {
+                    color : ${textColor};
+                }
+                `}
+            }
+        `}
+        span {
+            font-size: 3.5em;
+            color: ${pressed ? textColor : buttonColor};
+        }
     `;
     return (
         <Outer onClick={onClick}>
-            <Text>Z</Text>
+            <span>Z</span>
         </Outer>
     );
 };
