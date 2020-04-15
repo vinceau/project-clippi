f5ad6071f7afdc4f6e1bc39ee5a47c1d1b2fbac8
Vince Au <vinceau09@gmail.com>
2020-04-15 21:51:12 +1000

    Add dragging styles to playback item

--- src/renderer/components/recorder/PlaybackQueueItem.tsx
+++ src/renderer/components/recorder/PlaybackQueueItem.tsx
@@ -4,19 +4,44 @@ import { Icon } from "semantic-ui-react";
 import styled from "styled-components";
 import { transparentize } from "polished";
 import { Labelled } from "../Labelled";
+import { ThemeMode, useTheme } from "@/styles";
+import { lighten, darken } from "polished";
 
 import { DolphinEntry } from "@vinceau/slp-realtime";
 import { Draggable } from "react-beautiful-dnd";
 
-const Outer = styled.div`
+const Outer = styled.div<{
+    themeName: string;
+    isDragging: boolean;
+}>`
 display: flex;
 flex-direction: row;
 justify-content: space-between;
 align-items: center;
 padding: 10px;
 border-bottom: solid 1px ${p => transparentize(0.8, p.theme.foreground)};
-&:last-child {
-    border-bottom: none;
+background-color: ${p => {
+    const adjust = p.themeName === ThemeMode.DARK ? lighten : darken;
+    return adjust(0.1, p.theme.background);
+}};
+.remove-icon {
+    cursor: pointer;
+    opacity: 0;
+    transition: all 0.4s ease-in-out;
+    padding: 5px;
+}
+${p => p.isDragging && `
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    &&&,
+`}
+&:hover {
+    background-color: ${p => {
+        const adjust = p.themeName === ThemeMode.DARK ? lighten : darken;
+        return adjust(0.15, p.theme.background);
+    }};
+    .remove-icon {
+        opacity: 1;
+    }
 }
 `;
 
@@ -51,15 +76,17 @@ export const PlaybackQueueItem: React.FC<{
     file: DolphinEntry;
     onRemove?: () => void;
 }> = props => {
+    const theme = useTheme();
     const { index, file, onRemove } = props;
     const basename = path.basename(file.path);
     return (
         <Draggable draggableId={JSON.stringify(file)} index={index}>
-            {provided => (
-                <Outer
+            {(provided, snapshot) => (
+                <Outer themeName={theme.themeName}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
+                    isDragging={snapshot.isDragging}
                 >
                     <Details>
                         <Icon size="big" name="file outline" />
@@ -68,7 +95,11 @@ export const PlaybackQueueItem: React.FC<{
                             <span>{file.path}</span>
                         </DetailsContent>
                     </Details>
-                    <Labelled title="Remove"><Icon link size="large" name="close" onClick={onRemove} /></Labelled>
+                    <Labelled title="Remove">
+                        <div className="remove-icon" onClick={onRemove}>
+                            <Icon size="large" name="close" />
+                        </div>
+                    </Labelled>
                 </Outer>
             )}
         </Draggable>
