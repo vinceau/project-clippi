cbb8bb5e809f10bf2e6bbae7294d9bc40b03f04c
Vince Au <vinceau09@gmail.com>
2020-04-20 20:32:35 +1000

    Adjust playback settings form styles

---
+++ src/renderer/components/Form.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PageHeader = styled.h1`
+font-variant: all-small-caps;
+margin-bottom: 20px;
+`;
+
+export const Label = styled.div`
+&&& {
+font-weight: 500;
+font-size: 14px;
+margin-bottom: 10px;
+}
+`;
+
+export const Text = styled.p`
+font-size: 12px;
+margin-top: 10px;
+opacity: 0.8;
+`;
--- src/renderer/containers/Settings/PlaybackSettings.tsx
+++ src/renderer/containers/Settings/PlaybackSettings.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 
+import { Form } from "semantic-ui-react";
+
+import { FileInput } from "@/components/FileInput";
+
+import styled from "styled-components";
+import { Label, Text, PageHeader } from "@/components/Form";
+
+const Outer = styled.div`
+max-width: 800px;
+`;
+
 export const PlaybackSettings: React.FC = () => {
+    const [meleeIsoPath, setMeleeIsoPath] = React.useState("");
+    const [dolphinPath, setDolphinPath] = React.useState("");
     return (
-        <div>
-            Hello world!
-        </div>
+        <Outer>
+            <PageHeader>Playback</PageHeader>
+            <Form>
+                <Form.Field>
+                    <Label>Melee ISO File</Label>
+                    <FileInput
+                        value={meleeIsoPath}
+                        onChange={setMeleeIsoPath}
+                    />
+                    <Text>The path to an NTSC Melee 1.02 ISO. Dolphin will auto-launch this title when playing back replays.</Text>
+                </Form.Field>
+                <Form.Field>
+                    <Label>Playback Dolphin Path</Label>
+                    <FileInput
+                        value={dolphinPath}
+                        onChange={setDolphinPath}
+                    />
+                    <Text>The path to the Dolphin playback executable. You should only need to set this on Linux machines, or if you really know what you're doing.</Text>
+                </Form.Field>
+            </Form>
+            <p>{meleeIsoPath}</p>
+        </Outer>
     );
 };
