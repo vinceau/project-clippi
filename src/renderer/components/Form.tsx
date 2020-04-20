eaade55aeea7c9525a5298c7fb19256283dc13ec
Vince Au <vinceau09@gmail.com>
2020-04-20 22:20:00 +1000

    Add custom Field element

--- src/renderer/components/Form.tsx
+++ src/renderer/components/Form.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const PageHeader = styled.h1`
 font-variant: all-small-caps;
-margin-bottom: 20px;
+margin-bottom: 10px;
 `;
 
 export const Label = styled.div`
@@ -17,4 +17,20 @@ export const Text = styled.p`
 font-size: 12px;
 margin-top: 10px;
 opacity: 0.8;
+`;
+
+export const Field = styled.div<{
+    border?: string;
+}>`
+padding-top: 20px;
+padding-bottom: 20px;
+
+${p => (p.border === "top" || p.border === "both") && `
+border-top: solid 1px ${p.theme.foreground3};
+`}
+
+${p => (p.border === "bottom" || p.border === "both") && `
+border-bottom: solid 1px ${p.theme.foreground3};
+`}
+
 `;
--- src/renderer/containers/Settings/PlaybackSettings.tsx
+++ src/renderer/containers/Settings/PlaybackSettings.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import { Form } from "semantic-ui-react";
-
 import { FileInput } from "@/components/FileInput";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch, iRootState } from "@/store";
 import styled from "styled-components";
-import { Label, Text, PageHeader } from "@/components/Form";
+import { Label, Field, Text, PageHeader } from "@/components/Form";
 import { getDolphinExecutableName } from "@/lib/dolphin";
 
 const Outer = styled.div`
@@ -22,25 +20,23 @@ export const PlaybackSettings: React.FC = () => {
     return (
         <Outer>
             <PageHeader>Playback</PageHeader>
-            <Form>
-                <Form.Field>
-                    <Label>Melee ISO File</Label>
-                    <FileInput
-                        value={meleeIsoPath}
-                        onChange={setMeleeIsoPath}
-                    />
-                    <Text>The path to an NTSC Melee 1.02 ISO. Dolphin will auto-launch this title when playing back replays.</Text>
-                </Form.Field>
-                <Form.Field>
-                    <Label>Playback Dolphin Path</Label>
-                    <FileInput
-                        value={dolphinPath}
-                        directory={true}
-                        onChange={setDolphinPath}
-                    />
-                    <Text>The folder containing a <b>{getDolphinExecutableName()}</b> executable. You should only need to set this on Linux machines, or if you really know what you're doing.</Text>
-                </Form.Field>
-            </Form>
+            <Field>
+                <Label>Melee ISO File</Label>
+                <FileInput
+                    value={meleeIsoPath}
+                    onChange={setMeleeIsoPath}
+                />
+                <Text>The path to an NTSC Melee 1.02 ISO. Dolphin will auto-launch this title when playing back replays.</Text>
+            </Field>
+            <Field border="top">
+                <Label>Playback Dolphin Path</Label>
+                <FileInput
+                    value={dolphinPath}
+                    directory={true}
+                    onChange={setDolphinPath}
+                />
+                <Text>The folder containing the <b>{getDolphinExecutableName()}</b> playback executable. You should only need to set this on Linux machines, or if you really know what you're doing.</Text>
+            </Field>
         </Outer>
     );
 };
