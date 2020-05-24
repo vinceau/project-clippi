4354bdb823d0c3700e27471fd87f846fcc658027
Vince Au <vinceau09@gmail.com>
2020-05-24 14:42:39 +1000

    Fix combo filter not displaying the correct values

--- src/renderer/store/transformer.ts
+++ src/renderer/store/transformer.ts
@@ -4,11 +4,25 @@ import { produce } from "immer";
 import { createTransform } from "redux-persist";
 import { FileSystemState } from "./models/filesystem";
 import { TwitchState } from "./models/twitch";
+import { SlippiState } from "./models/slippi";
+import { mapConfigurationToFilterSettings, mapFilterSettingsToConfiguration } from "@/lib/profile";
+import { defaultComboFilterSettings } from "@vinceau/slp-realtime";
 
 export const transformer = createTransform(
     state => state,
     (state, key) => produce(state, draft => {
         switch (key) {
+            case "slippi":
+                // Repopulate combo profiles with default values
+                const { comboProfiles } = (draft as SlippiState);
+                for (const [key, value] of Object.entries(comboProfiles)) {
+                    // Get the current stored profiles
+                    const converted = mapConfigurationToFilterSettings(JSON.parse(value));
+                    // Join it with the default settings
+                    const newConverted = Object.assign({}, defaultComboFilterSettings, converted);
+                    (draft as SlippiState).comboProfiles[key] = JSON.stringify(mapFilterSettingsToConfiguration(newConverted))
+                }
+                break;
             case "twitch":
                 // Rehydrate the Date object for timestamps
                 const clips = (draft as TwitchState).clips;
