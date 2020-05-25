5db753c4c99ddc073d37c7efdf40376d1b0d0a93
Vince Au <vinceau09@gmail.com>
2020-05-25 23:43:00 +1000

    Remove unused import

--- src/renderer/components/combos/PortSelection.tsx
+++ src/renderer/components/combos/PortSelection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Field, FieldProps } from "react-final-form";
+import { Field } from "react-final-form";
 import { Checkbox, Grid, GridColumnProps } from "semantic-ui-react";
 
 export interface PortSelectionProps {
