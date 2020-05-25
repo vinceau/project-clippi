8805bdbe98207c27cae9b7d162072f4109be3c0d
Vince Au <vinceau09@gmail.com>
2020-05-25 23:33:31 +1000

    Migrate more styles to emotion

--- src/renderer/components/CustomIcon.tsx
+++ src/renderer/components/CustomIcon.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
-import styled, { css } from "styled-components";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
 
 export const CustomIcon: React.FC<{
   image: string;
