4e39bbbd3799e1a830e8fcb71675ff6cc8939a54
Vince Au <vinceau09@gmail.com>
2020-07-13 12:10:00 +1000

    Move automator actions into automator store

--- src/renderer/store/models/automator.ts
+++ src/renderer/store/models/automator.ts
@@ -3,6 +3,7 @@ import produce from "immer";
 import { createModel } from "@rematch/core";
 
 import { EventConfig } from "@vinceau/slp-realtime";
+import { Action as ActionDefinition } from "@vinceau/event-actions";
 
 export interface NamedEventConfig extends EventConfig {
   name: string;
@@ -10,10 +11,14 @@ export interface NamedEventConfig extends EventConfig {
 
 export interface AutomatorState {
   events: NamedEventConfig[];
+  actions: {
+    [eventId: string]: ActionDefinition[];
+  };
 }
 
 const initialState: AutomatorState = {
   events: [],
+  actions: {},
 };
 
 export const automator = createModel({
@@ -34,7 +39,46 @@ export const automator = createModel({
       }),
     removeEvent: (state: AutomatorState, payload: number): AutomatorState =>
       produce(state, (draft) => {
+        const eventId = draft.events[payload].id;
         draft.events.splice(payload, 1);
+        // Remove all the associated actions
+        delete draft.actions[eventId];
       }),
+    addNewEventAction: (
+      state: AutomatorState,
+      payload: { eventId: string; action: ActionDefinition }
+    ): AutomatorState => {
+      const { eventId, action } = payload;
+      const newActions = produce(state.actions, (draft) => {
+        if (!draft[eventId]) {
+          draft[eventId] = [];
+        }
+        draft[eventId].push(action);
+      });
+      return produce(state, (draft) => {
+        draft.actions = newActions;
+      });
+    },
+    updateActionEvent: (
+      state: AutomatorState,
+      payload: { eventId: string; index: number; action: ActionDefinition }
+    ): AutomatorState => {
+      const { eventId, index, action } = payload;
+      const newActions = produce(state.actions, (draft) => {
+        draft[eventId][index] = action;
+      });
+      return produce(state, (draft) => {
+        draft.actions = newActions;
+      });
+    },
+    removeActionEvent: (state: AutomatorState, payload: { eventId: string; index: number }): AutomatorState => {
+      const { eventId, index } = payload;
+      const newActions = produce(state.actions, (draft) => {
+        draft[eventId].splice(index, 1);
+      });
+      return produce(state, (draft) => {
+        draft.actions = newActions;
+      });
+    },
   },
 });
