4ba26272a5a345bb9ad7732fbaffa1e7c02929a9
Vince Au <vinceau09@gmail.com>
2020-07-14 17:52:41 +1000

    Fix compilation issues

--- src/renderer/lib/realtime.ts
+++ src/renderer/lib/realtime.ts
@@ -1,6 +1,4 @@
-import { Action, Context } from "@vinceau/event-actions";
 import {
-  ComboFilter,
   ConnectionStatus,
   SlpFolderStream,
   SlpLiveStream,
@@ -8,135 +6,10 @@ import {
   EventManager,
   EventManagerConfig,
 } from "@vinceau/slp-realtime";
-import { withLatestFrom } from "rxjs/operators";
 
 import { dispatcher } from "@/store";
-import {
-  exampleComboType,
-  exampleDeathStockType,
-  exampleGameEnd,
-  exampleGameStart,
-  exampleSpawnStockType,
-  generateComboContext,
-  generateGameEndContext,
-  generateGameStartContext,
-  generateGlobalContext,
-  generateStockContext,
-} from "common/context";
-import { isDevelopment } from "common/utils";
 import { eventActionManager } from "../containers/actions";
 import { notify } from "./utils";
-
-/*
-export enum ActionEvent {
-  GAME_START = "game-start",
-  GAME_END = "game-end",
-  PLAYER_SPAWN = "player-spawn",
-  PLAYER_DIED = "player-died",
-  COMBO_OCCURRED = "combo-occurred",
-  CONVERSION_OCCURRED = "conversion-occurred",
-}
-
-const errorHandler = (err: any) => {
-  console.error(err);
-  notify("Unexpected error occurred!");
-};
-
-export const comboFilter = new ComboFilter();
-if (isDevelopment) {
-  comboFilter.updateSettings({ excludeCPUs: false, comboMustKill: false, minComboPercent: 40 });
-}
-
-// const slippiRealtime = new SlpRealTime();
-
-slippiRealtime.game.start$.subscribe(async (gameStart) => {
-  try {
-    const ctx = generateGameStartContext(gameStart);
-    await eventActionManager.emitEvent(ActionEvent.GAME_START, generateGlobalContext(ctx));
-  } catch (err) {
-    errorHandler(err);
-  }
-});
-
-slippiRealtime.game.end$.subscribe(async (gameEnd) => {
-  try {
-    const ctx = generateGameEndContext(gameEnd);
-    await eventActionManager.emitEvent(ActionEvent.GAME_END, generateGlobalContext(ctx));
-  } catch (err) {
-    errorHandler(err);
-  }
-});
-
-slippiRealtime.stock.playerSpawn$
-  .pipe(withLatestFrom(slippiRealtime.game.start$))
-  .subscribe(async ([stock, settings]) => {
-    try {
-      const ctx = generateStockContext(stock, settings);
-      await eventActionManager.emitEvent(ActionEvent.PLAYER_SPAWN, generateGlobalContext(ctx));
-    } catch (err) {
-      errorHandler(err);
-    }
-  });
-
-slippiRealtime.stock.playerDied$
-  .pipe(withLatestFrom(slippiRealtime.game.start$))
-  .subscribe(async ([stock, settings]) => {
-    try {
-      const ctx = generateStockContext(stock, settings);
-      await eventActionManager.emitEvent(ActionEvent.PLAYER_DIED, generateGlobalContext(ctx));
-    } catch (err) {
-      errorHandler(err);
-    }
-  });
-
-slippiRealtime.combo.end$.subscribe(async (payload) => {
-  const { combo, settings } = payload;
-  try {
-    if (comboFilter.isCombo(combo, settings)) {
-      const ctx = generateComboContext(combo, settings);
-      await eventActionManager.emitEvent(ActionEvent.COMBO_OCCURRED, generateGlobalContext(ctx));
-    }
-  } catch (err) {
-    errorHandler(err);
-  }
-});
-
-slippiRealtime.combo.conversion$.subscribe(async (payload) => {
-  const { combo, settings } = payload;
-  try {
-    if (comboFilter.isCombo(combo, settings)) {
-      const ctx = generateComboContext(combo, settings);
-      await eventActionManager.emitEvent(ActionEvent.CONVERSION_OCCURRED, generateGlobalContext(ctx));
-    }
-  } catch (err) {
-    errorHandler(err);
-  }
-});
-
-export const testRunActions = (event: string, actions: Action[]): void => {
-  console.log(`testing ${event} event`);
-  let ctx: Context = {};
-  switch (event) {
-    case ActionEvent.GAME_START:
-      ctx = generateGameStartContext(exampleGameStart);
-      break;
-    case ActionEvent.GAME_END:
-      ctx = generateGameEndContext(exampleGameEnd);
-      break;
-    case ActionEvent.PLAYER_SPAWN:
-      ctx = generateStockContext(exampleSpawnStockType, exampleGameStart);
-      break;
-    case ActionEvent.PLAYER_DIED:
-      ctx = generateStockContext(exampleDeathStockType, exampleGameStart);
-      break;
-    case ActionEvent.COMBO_OCCURRED:
-    case ActionEvent.CONVERSION_OCCURRED:
-      ctx = generateComboContext(exampleComboType, exampleGameStart);
-      break;
-  }
-  eventActionManager.execute(actions, generateGlobalContext(ctx)).catch(console.error);
-};
-*/
 
 class SlpStreamManager {
   private stream: SlpLiveStream | SlpFolderStream | null = null;
