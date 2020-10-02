715c1de357ab423c95f3acd8aa78009af520f006
Vince Au <vinceau09@gmail.com>
2020-10-02 11:38:09 +1000

    Remove unused imports

--- src/renderer/containers/SlippiConnectionPlaceholder.tsx
+++ src/renderer/containers/SlippiConnectionPlaceholder.tsx
@@ -2,58 +2,24 @@ import * as React from "react";
 
 import styled from "@emotion/styled";
 
-import { useDispatch, useSelector } from "react-redux";
 import { Button, Divider, Grid, Header, Input, Segment } from "semantic-ui-react";
 
 import { CustomIcon } from "@/components/CustomIcon";
-import { BufferedInput } from "@/components/InlineInputs";
-import { streamManager } from "@/lib/realtime";
-import { getFolderPath } from "@/lib/utils";
-import { Dispatch, dispatcher, iRootState } from "@/store";
+import { dispatcher } from "@/store";
 import { device } from "@/styles/device";
-
 import { SlippiIcon } from "@/components/SlippiIcon";
+
 import dolphinLogoSVG from "@/styles/images/dolphin.svg";
-import { Ports } from "@slippi/slippi-js";
 
 export const SlippiConnectionPlaceholder: React.FC<{
   port: string;
   onClick: (port: string) => void;
 }> = (props) => {
-  const { liveSlpFilesPath } = useSelector((state: iRootState) => state.filesystem);
-  const dispatch = useDispatch<Dispatch>();
-  const selectPath = async () => {
-    const filepath = await getFolderPath();
-    if (filepath) {
-      dispatch.filesystem.setLiveSlpFilesPath(filepath);
-    }
-  };
   const [port, setPort] = React.useState(props.port);
   const VerticalHeader = styled(Header)`
     &&& {
       display: flex;
       flex-direction: column;
-    }
-  `;
-  const FolderInput = styled.div`
-    &&& {
-      display: flex;
-      flex-direction: column;
-      padding: 0 10px;
-      @media ${device.laptop} {
-        flex-direction: row;
-        padding: 0 30px;
-      }
-    }
-  `;
-  const ButtonContainer = styled.div`
-    &&& {
-      margin-top: 5px;
-      margin-left: 0px;
-      @media ${device.laptop} {
-        margin-top: 0px;
-        margin-left: 5px;
-      }
     }
   `;
   const VerticalDivider = styled(Divider)`
@@ -73,7 +39,6 @@ export const SlippiConnectionPlaceholder: React.FC<{
       }
     }
   `;
-  const setSlpFilePath = (filepath: string) => dispatch.filesystem.setLiveSlpFilesPath(filepath);
   return (
     <Segment placeholder>
       <Grid columns={2} stackable textAlign="center">
