a04ad0ab0036a2ad4caa723791688c1a579d47a5
Vince Au <vinceau09@gmail.com>
2020-12-09 10:35:27 +1100

    Reduce end buffer when finding highlights

--- src/common/fileProcessor.ts
+++ src/common/fileProcessor.ts
@@ -195,7 +195,9 @@ export class FileProcessor {
     let totalCombos = 0;
     if (opts.findComboOption && opts.outputFile) {
       totalCombos = this.queue.length;
-      const payload = generateDolphinQueuePayload(this.queue);
+      const payload = generateDolphinQueuePayload(this.queue, {
+        endBuffer: 30, // Additional frames to capture after the combo ends
+      });
       await fs.writeFile(opts.outputFile, payload);
       console.log(`Wrote ${totalCombos} out to ${opts.outputFile}`);
     }
