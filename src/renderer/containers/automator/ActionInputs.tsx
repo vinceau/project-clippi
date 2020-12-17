feb606357c6c4b5832aab75b9ecb8c50eda62f2e
Vince Au <vinceau09@gmail.com>
2020-12-17 21:19:15 +1100

    Move ActionComponentBlock into separate file

--- src/renderer/containers/automator/ActionInputs.tsx
+++ src/renderer/containers/automator/ActionInputs.tsx
@@ -1,8 +1,4 @@
-/** @jsx jsx */
-import { css, jsx } from "@emotion/core";
 import React from "react";
-
-import styled from "@emotion/styled";
 
 import { Action as ActionDefinition } from "@vinceau/event-actions";
 import { produce } from "immer";
@@ -12,6 +8,7 @@ import { InlineDropdown } from "@/components/InlineInputs";
 import { Labelled } from "@/components/Labelled";
 import { actionComponents } from "@/containers/actions";
 import { ActionIcon } from "./ActionIcon";
+import { ActionComponentBlock } from "./ActionComponentBlock";
 
 const allActions = Object.keys(actionComponents);
 
@@ -24,53 +21,6 @@ const ActionSelector = (props: any) => {
       mapOptionToLabel={(opt: string) => actionComponents[opt].label}
       fontSize={18}
     />
-  );
-};
-/*
-      {
-        name: string;
-        transform?: boolean;
-        args?: any;
-      }
-*/
-
-const ActionComponentBlock = (props: any) => {
-  const { hideBorder, icon, header, children, ...rest } = props;
-  const Outer = styled.div`
-    margin: 2rem;
-    padding: 2rem;
-    padding-top: 0;
-    display: flex;
-    flex-direction: column;
-    ${({ theme }) => (hideBorder ? "" : `border-bottom: solid 0.3rem ${theme.foreground3}`)};
-  `;
-  const Content = styled.div`
-    width: 100%;
-  `;
-  const ActionDetails = styled.div`
-    line-height: 3rem;
-  `;
-  return (
-    <Outer {...rest}>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          ${!hideBorder && `padding-bottom: 1rem;`}
-        `}
-      >
-        <div
-          css={css`
-            margin-right: 1rem;
-          `}
-        >
-          {icon}
-        </div>
-        <div>{header}</div>
-      </div>
-      <Content>{children && <ActionDetails>{children}</ActionDetails>}</Content>
-    </Outer>
   );
 };
 
@@ -102,25 +52,24 @@ export const ActionInput: React.FC<{
   }
   const ActionArgsInput = actionComponents[value.name].Component;
   return (
-    <div ref={outerRef}>
-      <ActionComponentBlock
-        icon={
-          <Labelled title="Remove" onClick={onRemove}>
-            <ActionIcon name={value.name} outer={outerRef} />
-          </Labelled>
-        }
-        header={
-          <ActionSelector
-            prefix={selectPrefix}
-            value={value.name}
-            onChange={onActionChange}
-            disabledOptions={disabledActions}
-          />
-        }
-      >
-        <ActionArgsInput value={value.args} onChange={onArgsChange} />
-      </ActionComponentBlock>
-    </div>
+    <ActionComponentBlock
+      ref={outerRef}
+      icon={
+        <Labelled title="Remove" onClick={onRemove}>
+          <ActionIcon name={value.name} outer={outerRef} />
+        </Labelled>
+      }
+      header={
+        <ActionSelector
+          prefix={selectPrefix}
+          value={value.name}
+          onChange={onActionChange}
+          disabledOptions={disabledActions}
+        />
+      }
+    >
+      <ActionArgsInput value={value.args} onChange={onArgsChange} />
+    </ActionComponentBlock>
   );
 };
 
@@ -132,10 +81,13 @@ export const AddActionInput: React.FC<{
   const unusedOptions = allActions.filter((a) => !disabledActions.includes(a));
   const noOtherActions = unusedOptions.length === allActions.length;
   const addText = noOtherActions ? "Then..." : "And also...";
+  if (unusedOptions.length === 0) {
+    return null;
+  }
+
   return (
     <ActionComponentBlock
       hideBorder={true}
-      style={unusedOptions.length === 0 ? { display: "none" } : undefined}
       icon={<Icon name="add" size="large" />}
       header={<ActionSelector text={addText} selectOnBlur={false} onChange={onChange} options={unusedOptions} />}
     />
