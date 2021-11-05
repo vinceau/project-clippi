16dd10d947507ed40bee48b3ca5550a660a78a0b
Vince Au <vinceau09@gmail.com>
2021-11-05 16:55:54 +1100

    feat: simplify exists function

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -145,6 +145,9 @@ const validFilename = (name: string): boolean => {
   return true;
 };
 
-export function exists<T>(t: T | null | undefined): t is T {
-  return t !== null && t !== undefined;
+// Based on https://github.com/wilsonzlin/edgesearch/blob/d03816dd4b18d3d2eb6d08cb1ae14f96f046141d/demo/wiki/client/src/util/util.ts
+// Ensures value is not null or undefined.
+// != does no type validation so we don't need to explcitly check for undefined.
+export function exists<T>(value: T | null | undefined): value is T {
+  return value != null;
 }
