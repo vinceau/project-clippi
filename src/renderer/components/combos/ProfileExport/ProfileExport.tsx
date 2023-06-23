9a1056229b83dd5f963ca25ea913e8e551767329
Vince Au <vinceau09@gmail.com>
2023-06-23 23:54:39 +1000

    swap import and export profile icons

--- src/renderer/components/combos/ProfileExport/ProfileExport.tsx
+++ src/renderer/components/combos/ProfileExport/ProfileExport.tsx
@@ -5,8 +5,8 @@ export const ProfileExport = ({ onImport, onExport }: { onImport: () => void; on
   return (
     <Dropdown icon="bars" floating button className="icon">
       <Dropdown.Menu>
-        <Dropdown.Item icon="upload" text="Import profile" onClick={onImport} />
-        <Dropdown.Item icon="download" text="Export profile" onClick={onExport} />
+        <Dropdown.Item icon="download" text="Import profile" onClick={onImport} />
+        <Dropdown.Item icon="upload" text="Export profile" onClick={onExport} />
       </Dropdown.Menu>
     </Dropdown>
   );
