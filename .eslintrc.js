39ebc2d00aad2e00d425b05c0ccc56a3694ba773
Vince Au <vinceau09@gmail.com>
2026-05-14 13:34:56 +0900

    add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'error',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
+    "import/prefer-default-export": "off",
   },
   parserOptions: {
     ecmaVersion: 2022,
