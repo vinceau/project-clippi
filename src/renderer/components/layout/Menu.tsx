685cec221a1f8be9304fa807e668eb9634cb2acb
Vince Au <vinceau09@gmail.com>
2026-05-15 22:02:45 +0900

    fix incorrect icons in menu

--- src/renderer/components/layout/Menu.tsx
+++ src/renderer/components/layout/Menu.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link, useRouteMatch } from "react-router-dom";
-import { Bolt, ChevronsRight, CirclePlay, Settings } from "lucide-react";
+import { Zap, ChevronsRight, CirclePlay, Settings } from "lucide-react";
 import { Label } from "@/ui/Label/Label";
 
 import { MenuIcon, MenuIconLink } from "@/components/layout/MenuIcon";
@@ -13,19 +13,19 @@ export function Menu({ settingsPage, updateAvailable }: { settingsPage: string; 
     <div className={styles.outer}>
       <div>
         <MenuIconLink to={`${match.url}/automator`} label="Automator">
-          <Bolt size={20} />
+          <Zap size={30} />
         </MenuIconLink>
         <MenuIconLink to={`${match.url}/processor`} label="Replay Processor">
-          <ChevronsRight size={20} />
+          <ChevronsRight size={30} />
         </MenuIconLink>
         <MenuIconLink to={`${match.url}/recorder`} label="Playback Queue">
-          <CirclePlay size={20} />
+          <CirclePlay size={30} />
         </MenuIconLink>
       </div>
       <div>
         <Link to={settingsPage}>
           <MenuIcon label="Settings">
-            <Settings size={20} />
+            <Settings size={30} />
             {updateAvailable && (
               <span style={{ position: "absolute", top: "2rem", right: "2rem" }}>
                 <Label circular color="red" empty />
