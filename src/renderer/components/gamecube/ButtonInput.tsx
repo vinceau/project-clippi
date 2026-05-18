1816d2b7d74a6b057b6a446b38b1251bfdce0808
Vince Au <vinceau09@gmail.com>
2026-05-18 12:50:04 +0900

    remove wrong style prop

--- src/renderer/components/gamecube/ButtonInput.tsx
+++ src/renderer/components/gamecube/ButtonInput.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import styles from "./ButtonInput.module.css";
 import { Tooltip } from "@/ui/Tooltip/Tooltip";
+import styles from "./ButtonInput.module.css";
 import { ButtonPicker } from "./ButtonPicker";
 import { ButtonPreview } from "./ButtonPreview";
 
 export function ButtonInput({ value, onChange }: { value?: string[]; onChange?: (newButtons: string[]) => void }) {
   return (
     <ButtonPicker value={value} onChange={onChange}>
-      <Tooltip title="Click to select a button combination" style={{ width: "100%" }}>
+      <Tooltip title="Click to select a button combination" triggerClassName={styles.fullWidth}>
         <div className={styles.target}>
           {value && value.length > 0 ? (
             <ButtonPreview value={value} pressed />
