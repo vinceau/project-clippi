ac91bed2361409fee54f3ec5e1170df778843bd7
Vince Au <vinceau09@gmail.com>
2026-05-20 23:13:19 +0900

    chore: update declarations

--- src/renderer/declarations.d.ts
+++ src/renderer/declarations.d.ts
@@ -1,6 +1,39 @@
-declare module "*.svg";
-declare module "*.png";
-declare module "*.jpg";
+type Styles = Record<string, string>;
+
+declare module "*.svg" {
+  import React = require("react");
+
+  export const ReactComponent: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement | null;
+
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: Styles;
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: Styles;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: Styles;
+  export default content;
+}
+
 declare module "*.jpeg";
 declare module "*.gif";
 declare module "*.bmp";
@@ -9,11 +42,9 @@ declare module "*.tiff";
 declare module "*.md";
 declare module "*.module.css";
 
-declare module "node-notifier";
 declare module "filename-reserved-regex";
 declare module "insert-text-at-cursor";
 declare module "react-beautiful-dnd";
-declare module "sinon";
 
 declare module "raw-loader!*.md" {
   const content: string;
