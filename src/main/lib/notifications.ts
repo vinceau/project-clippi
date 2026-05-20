66860dc4fe325576bc0bd5b1f108bdf96b2164c3
Vince Au <vinceau09@gmail.com>
2026-05-20 16:17:25 +0900

    fix notifications

--- src/main/lib/notifications.ts
+++ src/main/lib/notifications.ts
@@ -1,59 +1,35 @@
-import { IS_DEV } from "common/constants";
 import type { NotificationConstructorOptions } from "electron";
 import { app, BrowserWindow, Notification } from "electron";
-import notifier from "node-notifier";
-import * as path from "path";
+import pkg from "../../../package.json";
 
-export const showNotification = (message: string, title?: string): void => {
+if (process.platform === "win32") {
+  app.setAppUserModelId(pkg.build.appId);
+}
+
+export async function showNotification(message: string, title?: string): Promise<void> {
+  await app.whenReady();
+
   const notificationTitle = title || "Project Clippi";
-  switch (process.platform) {
-    case "win32":
-      showWindowsNotification(message, notificationTitle);
-      break;
-    default:
-      showElectronNotification(message, notificationTitle);
-      break;
-  }
-};
 
-const showWindowsNotification = (message: string, title: string): void => {
-  const fileIcon = getStatic(`/images/icon.png`);
-  notifier.notify({
-    title,
-    message,
-    icon: path.resolve(fileIcon), // Don't use SnoreToast icon
-    id: 1, // Setting this ID lets us dismiss existing notifications immediately
-  });
-};
-
-const showElectronNotification = (message: string, title: string): void => {
   const options: NotificationConstructorOptions = {
-    title,
+    title: notificationTitle,
     body: message,
   };
 
-  // Don't set the icon on MacOS or it will show up twice
-  if (process.platform !== "darwin") {
-    options.icon = getStatic(`/images/icon.png`);
-  }
+  const notification = new Notification(options);
 
-  const n = new Notification(options);
-  // Show the main window on click
-  const clickHandler = () => {
-    const win = BrowserWindow.getFocusedWindow();
-    if (win) {
-      win.show();
+  notification.on("click", () => {
+    const win = BrowserWindow.getAllWindows()[0];
+
+    if (!win) return;
+
+    if (win.isMinimized()) {
+      win.restore();
     }
-  };
-  n.on("click", clickHandler);
-  n.show();
-};
 
-const getStatic = (val: string): string => {
-  if (IS_DEV) {
-    return path.join(__dirname, "../../../static", val);
-  }
-  const appPath = app.getAppPath();
-  const imagePath = path.join(appPath, "../static");
-  return path.resolve(path.join(imagePath, val));
-};
+    win.show();
+    win.focus();
+  });
+
+  notification.show();
+}
